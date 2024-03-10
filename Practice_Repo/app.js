3ce4e6aa925fd303a78f1949bbf4518ab170eb9f
kashifalismitofficial <kashifalismitofficail>
2024-03-10 17:18:34 +0500

    pracitve

---
+++ Practice_Repo/app.js
@@ -0,0 +1,78 @@
+// var userName = "Kashif Awan"; declare variable
+// var userName = "Kashif Ali";  redeclare variable
+// userName = "Mahii";   reassign variable
+// console.log(userName);
+
+
+
+
+// let firstName = "Kashif";
+// firstName ="Ali Awan";
+// console.log(firstName);
+
+
+
+
+
+// const userName ="kashif Ali jan";
+// userName = "just chk";
+// console.log(userName);
+
+
+
+
+
+
+
+// let city1 = 'karachi';
+// let city2 = 'lahore';
+// let city3 = 'islamabad';
+
+
+// let cities = ['karachi' , 'lahore' , 'islamabad' , 'faislabad' , 'hyderabad' , 'sheikarpur'];
+// let cities = ['karachi' , 'lahore' , 'islamabad' , 'faislabad' , 'hyderabad' , 'sheikarpur' , 20 , true , ['fb area' , 'liyari' , 'golimar']];
+// console.log(cities[8][2]);
+// console.log(cities.length);
+// console.log(cities[3]);
+// console.log(typeof(cities))
+
+
+
+
+
+// let KahsifBhanjay = ['Ubaidullah jani', 'Rehan jan', 'Ali kokay', 'Azan Jan' , 'Mujahid Ali', 'Abdul Hadii Baba']
+// console.log(typeof(KahsifBhanjay));
+// console.log(KahsifBhanjay);
+
+
+let Kashiffamilychild =['Ubaidullah jani', 'Rehan jan', 'Ali kokay', 'Azan Jan'  , ['Mujahid Ali', 'Abdul Hadii Baba']];
+console.log();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
