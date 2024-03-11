2d4b7f7a329b5a2ca47e83fe5b2cfc67a9550a0d
kashifalismit <kashifalismitofficial@gmail.com>
2024-03-11 17:22:02 +0500

    visual

--- Practice_Repo/app.js
+++ Practice_Repo/app.js
@@ -6,17 +6,17 @@
 
 
 
-let firstName = "Kashif";
-firstName ="Ali Awan";
-console.log(firstName);
+// let firstName = "Kashif";
+// firstName ="Ali Awan";
+// console.log(firstName);
 
 
 
 
 
-const userName ="kashif Ali jan";
-userName = "just chk";
-console.log(userName);
+// const userName ="kashif Ali jan";
+// userName = "just chk";
+// console.log(userName);
 
 
 
@@ -24,17 +24,17 @@ console.log(userName);
 
 
 
-let city1 = 'karachi';
-let city2 = 'lahore';
-let city3 = 'islamabad';
+// let city1 = 'karachi';
+// let city2 = 'lahore';
+// let city3 = 'islamabad';
 
 
 // let cities = ['karachi' , 'lahore' , 'islamabad' , 'faislabad' , 'hyderabad' , 'sheikarpur'];
-let cities = ['karachi' , 'lahore' , 'islamabad' , 'faislabad' , 'hyderabad' , 'sheikarpur' , 20 , true , ['fb area' , 'liyari' , 'golimar']];
-console.log(cities[8][2]);
-console.log(cities.length);
-console.log(cities[3]);
-console.log(typeof(cities))
+// let cities = ['karachi' , 'lahore' , 'islamabad' , 'faislabad' , 'hyderabad' , 'sheikarpur' , 20 , true , ['fb area' , 'liyari' , 'golimar']];
+// console.log(cities[8][2]);
+// console.log(cities.length);
+// console.log(cities[3]);
+// console.log(typeof(cities))
 
 
 
@@ -45,10 +45,29 @@ console.log(typeof(cities))
 // console.log(KahsifBhanjay);
 
 
-let Kashiffamilychild =['Ubaidullah jani', 'Rehan jan', 'Ali kokay', 'Azan Jan'  , ['Mujahid Ali', 'Abdul Hadii Baba']];
-console.log(Kashiffamilychild[4]);
+// let Kashiffamilychild =['Ubaidullah jani', 'Rehan jan', 'Ali kokay', 'Azan Jan'  , ['Mujahid Ali', 'Abdul Hadii Baba']];
+// console.log(Kashiffamilychild[4]);
 
 
+
+
+
+const shop = ['Apple', 'graps', 'mango', 'bnana', 'watermelon', 'salat'];
+let fruits = document.querySelector(`#fruits`);
+let head2 = document.querySelector(`#head-2`);
+
+
+
+
+function getFruits() {
+    console.log(fruits.value);
+    if (shop.includes(fruits.value)) {
+        console.log("Fruit in available");
+    } else {
+        console.log("not fruit in not available")
+    }
+
+}
 
 
 
