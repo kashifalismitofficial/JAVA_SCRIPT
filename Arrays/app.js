5312007927918e34fea591fba65980fa1f20f05d
kashifalismitofficial <kashifalismitofficail>
2024-03-17 17:15:56 +0500

    one and tow

---
+++ Arrays/app.js
@@ -0,0 +1,20 @@
+// 1. Declare an empty array using JS literal notation to store
+// student names in future.>>>>>>>>>
+
+// Ans  let futureStudentsName = [];
+    //  console.log=(futureStudents);
+
+
+// 2. Declare an empty array using JS object notation to store
+// student names in future.
+
+// Ans  let futureStudentNames = new Array();
+        // console.log(futureStudentNames);
+
+
+// 3. Declare and initialize a strings array        
+
+// let Strig = ["kashif ALi","Bashir Ahmed" ,"Awan"]
+// console.log(Strig)
+// console.log(typeof(Strig));
+// console.log(Strig.length);
