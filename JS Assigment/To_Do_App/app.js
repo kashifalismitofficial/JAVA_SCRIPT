61de265a9bf91b4f51b19d4228530773a36c61f8
kashifalismitofficial <kashifalismitofficail>
2024-03-21 02:25:11 +0500

    todo app complete

---
+++ JS Assigment/To_Do_App/app.js
@@ -0,0 +1,56 @@
+let inputTodo = document.querySelector(`#inputTodo`);
+let ol = document.querySelector(`ol`);
+let arr = [];
+
+function runTodo() {
+    ol.innerHTML = "";
+    arr.push(inputTodo.value);
+    inputTodo.value = "";
+    console.log(arr);
+
+    for (let i = 0; i < arr.length; i++) {
+        ol.innerHTML += `<li>${arr[i]}
+        <button onclick="deletTodo(${i})">Delete</button> <button onclick=
+        "editTodo(${i})">Edit</button>
+        </li>`
+
+    }
+
+
+
+}
+
+
+function deletTodo(index) {
+    ol.innerHTML="";
+    console.log(`delete todo`, index)
+    arr.splice(index , 1 )
+
+    for (let i = 0; i < arr.length; i++) {
+        ol.innerHTML += `<li>${arr[i]}
+        <button onclick="deletTodo(${i})">Delete</button> <button onclick=
+        "editTodo(${i})">Edit</button>
+        </li>`
+
+    }
+}
+
+function editTodo(index) {
+    ol.innerHTML=""
+    console.log(`edit todo`, index);
+    let updatedvalue = prompt("Enter Updated Value");
+    arr.splice(index,1,updatedvalue)
+    for (let i = 0; i < arr.length; i++) {
+        ol.innerHTML += `<li>${arr[i]}
+        <button onclick="deletTodo(${i})">Delete</button> <button onclick=
+        "editTodo(${i})">Edit</button>
+        </li>`
+
+    }
+}
+
+
+
+
+
+
