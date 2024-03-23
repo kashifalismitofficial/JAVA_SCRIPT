d96ed819ae4d99de44982e807f7f656c022e5967
kashifalismitofficial <kashifalismitofficail>
2024-03-23 18:23:38 +0500

    5

--- JS Assigment/For_Loops/app.js
+++ JS Assigment/For_Loops/app.js
@@ -1,7 +1,7 @@
 // For Loops Assigment
 
 // <<<<<<<<<<< Question No 01 >>>>>>>>>>>>
-//  Write a program to display the message “Hello World” 5 times 
+//  Write a program to display the message "Hello World” 5 times 
 // in your browser using for loop
 
 // Answer:
@@ -28,8 +28,6 @@
 // for (let i = 0; i < 10; i++) {
 //     ol.innerHTML +=`<li>${"Number"} ${i +1}</li>`
 //     // console.log(i+1);
-    
-    
 // }
 
 
@@ -46,5 +44,40 @@
 // let times = prompt("Enter Lenght");
 // for (let  i= 1;  i <= times ; i++) {
 //     console.log(`${table} * ${i} = ${table * i}`);
-    
 // }
+
+
+// <<<<<<<<<<< Question No 04 >>>>>>>>>>>>
+// You have an array
+// A = ["Nokia”, "Samsung”, "Apple”, "Sony”, "Huawei”]
+// Write each element on new line with the help of for loop.
+
+// Answer:
+
+// let A = ["Nokia", "Samsung", "Apple", "Sony", "Huawei"];
+// let arry = document.querySelector(`#arry`);
+
+// for (let i = 0; i < A.length; i++) {
+//     arry.innerHTML +=`<p>${A[i]}</p>`
+
+// }
+
+
+
+
+// <<<<<<<<<<< Question No 04 >>>>>>>>>>>>
+// Write a program to print items of the following array using for loop:
+// fruits = ["apple”, "banana”, "mango”, "orange”, "strawberry”]
+
+// Answer:
+
+let pro = document.querySelector(`#pro`);
+let content = document.querySelector(`#content`);
+let fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+
+for (let i = 0; i < fruits.length; i++) {
+    pro.innerHTML += `<p>${fruits[i]}</p>`
+    content.innerHTML += `<p>Element At Index ${i} is ${fruits[i]}  </p>`
+
+}
+
