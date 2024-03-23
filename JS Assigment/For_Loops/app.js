8f82b72f8a6e7d41d4ed46f45e908c3d9d6dd103
kashifalismitofficial <kashifalismitofficail>
2024-03-23 17:14:48 +0500

    question answer

---
+++ JS Assigment/For_Loops/app.js
@@ -0,0 +1,50 @@
+// For Loops Assigment
+
+// <<<<<<<<<<< Question No 01 >>>>>>>>>>>>
+//  Write a program to display the message “Hello World” 5 times 
+// in your browser using for loop
+
+// Answer:
+
+// let head2 = document.querySelector(`#head02`)
+// let hello = "Hello World";
+
+// for (let i = 0; i < 5; i++) {
+//     // console.log('hello world', i);
+//     head2.innerHTML += `<li>${hello} ${i +1}</li>`
+
+// }
+
+
+
+
+
+// <<<<<<<<<<< Question No 02 >>>>>>>>>>>>
+// 2. Write a program to print numeric counting from 1 to 10.
+
+// Answer:
+
+// let ol = document.querySelector(`#head02`)
+// for (let i = 0; i < 10; i++) {
+//     ol.innerHTML +=`<li>${"Number"} ${i +1}</li>`
+//     // console.log(i+1);
+    
+    
+// }
+
+
+
+
+// <<<<<<<<<<< Question No 03 >>>>>>>>>>>>
+// Write a program to print multiplication table of any number 
+// using for loop. Table number & length should be taken as an 
+// input from user.
+
+// Answer
+
+// let table = prompt("Enter Table");
+// let times = prompt("Enter Lenght");
+// for (let  i= 1;  i < times ; i++) {
+//     console.log(`${table} * ${i} = ${table * i+1}`);
+    
+// }
