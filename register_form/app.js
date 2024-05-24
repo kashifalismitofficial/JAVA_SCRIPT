5036737361bce9584c97cdd15a80498ab57c9fc1
kashifalismit <kashifalismitofficial@gmail.com>
2024-05-24 12:58:38 +0500

    complete

--- register_form/app.js
+++ register_form/app.js
@@ -4,13 +4,13 @@ const input01 = document.querySelector(`#firt`);
 const input02 = document.querySelector(`#second`);
 const input03 = document.querySelector(`#third`);
 const input04 = document.querySelector(`#fourth`);
-const input05 = document.querySelector(`five`);
+const input05 = document.querySelector(`#five`);
 
 let firstName = /^[A-Za-z0-9._%+-]{3,13}$/ ;
-let lastName = /^[a-zA-Z]{8,15}$/ ;
+let lastName = /^[a-zA-Z]{3,15}$/ ;
 let SchoolName =  /^.{15,50}$/ ;
 let email = /^[a-zA-Z0-9._%+-]+@gmail\.com$/ ;
-let cnicPattern = /^42\d{11,13}$/;
+let cnicPattern = /^42\d{11}$/;
 
 
 //
@@ -35,6 +35,11 @@ form.addEventListener('submit', function(event){
         if (cnicPattern.test(input05.value)){
             console.log(input05.value);
         }
+        input01.value =''
+        input02.value =''
+        input03.value =''
+        input04.value =''
+        input05.value =''
         
 })
 
