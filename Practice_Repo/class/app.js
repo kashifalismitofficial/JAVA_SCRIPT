e7003e32a7fe0354f1ef596661776522e0379280
kashifalismitofficial <kashifalismitofficail>
2024-05-26 12:25:53 +0500

    half without css

---
+++ Practice_Repo/class/app.js
@@ -0,0 +1,30 @@
+const hobbies = [
+    "Reading",
+    "Writing",
+    "Painting",
+    "Drawing",
+    "Playing an instrument",
+    "Gardening",
+    "Cooking",
+    "Photography",
+    "Hiking",
+    "Camping"];
+
+
+const obj = {
+    name: 'kahif ali',
+    fatherName: 'Bashir Ahmed',
+    age: 20
+}
+// hobbies.map((items,index)=>{
+//     console.log(items);
+//     console.log(index);
+// } )
+
+// for (const iterator of hobbies) {
+//     console.log(iterator);
+// }
+
+for (const key in obj) {
+   console.log(ob[key]);
+}
