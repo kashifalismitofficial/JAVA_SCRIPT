f971d763b4c8726bdc033d2f92c4ba30a25ef9ef
kashifalismitofficial <kashifalismitofficail>
2024-06-08 19:02:26 +0500

    login

--- Login_signup/index.js
+++ Login_signup/index.js
@@ -3,17 +3,31 @@ let fullName = document.querySelector('#Name');
 let email = document.querySelector('#email');
 let password = document.querySelector('#password');
 
+let registerPerson = [];
 
-form.addEventListener('submit', event =>{
+form.addEventListener('submit', event => {
     event.preventDefault();
+    registerPerson.push({
+        email: email.value,
+        name: fullName.value,
+        password: password.value
+    })
+    localStorage.setItem('items', JSON.stringify(registerPerson))
+    console.log(registerPerson);
+    if(registerPerson == )
+
+
+
+
+
+
+
     // console.log(fullName.value);
     // console.log(email.value);
     // console.log(password.value);
-    const saveItem = localStorage.setItem('sendItems', JSON.stringify(form.value))
-    console.log(saveItem);
-    // fullName.value=""
-    // email.value=""
-    // password.value=""
+    fullName.value=""
+    email.value=""
+    password.value=""
 
 
 
